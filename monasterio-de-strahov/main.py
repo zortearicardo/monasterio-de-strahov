58522d876aa5256f89172af70fc54f39aed70661
zortearicardo <zortea.ricardo@gmail.com>
2015-09-15 13:01:05 -0300

    Update main.py

--- monasterio-de-strahov/main.py
+++ monasterio-de-strahov/main.py
@@ -2,39 +2,61 @@ import webapp2
 import hashlib
 
 mainForm = """
-<html>
-  <head>
-    <title>Hello!</title>
-  </head>
-  <body>
-    <form method="post">
-    <h1>Monasterio de Strahov</h1>
+<html> 
+<head> 
+	<meta charset="utf-8" /> 	
+	<title>Monasterio de Strahov</title>
+</head> 
+<body> 
+<form name="meuForm" method="post" id="formulario"> 
+	<div class="box"> 
+		<h1>Cadastro de cliente:</h1>
+	<label>
+	<p> </p> 
+			<span>Nome:</span> <input type="text" class="input_text" name="nome" id="name"/> 
+	</label></br>
+	<p> </p> 	
+			<label> <span>Cpf:&nbsp&nbsp&nbsp &nbsp</span><input type="text" class="input_text" name="fone" id="fone"/> 
+	</label> </br>
+		<p> </p> 
+			<label> <span>Fone:  &nbsp  </span> <input type="text" class="input_text" name="fone" id="fone"/> 
+	</label> </br>
+		<p> </p> 
+			<label> <span>End:&nbsp&nbsp &nbsp</span> <input type="text" class="input_text" name="end" id="end"/> 
+	</label> </br>
+		<p> </p> 
+			<input type="button" name="botao-ok" value="Confirmar">
+			
+			
+			<h1>Cadastro De Livro:</h1>
+	<label>
+	<p> </p> 
+			<span>Titulo:&nbsp</span> <input type="text" class="input_text" name="titulo" id="titulo"/> 
+	</label></br>
+		<p> </p> 
+			<label> <span>Autor:&nbsp&nbsp&nbsp</span><input type="text" class="input_text" name="autor" id="autor"/> 
+	</label> </br>
+		<p> </p> 
+			<label> <span>Editora:</span> <input type="text" class="input_text" name="editora" id="editora"/> 
+	</label> </br>
+		<p> </p> 
+			<label> <span>Ano:&nbsp&nbsp &nbsp&nbsp</span> <input type="text" class="input_text" name="ano" id="ano"/> 
+	</label> </br>
+		<p> </p> 
+			<input type="button" name="botao-ok" value="Confirmar">
+		
+		
 
-    <label> 
-
-
-
-
-
-    
-      <input type="text" name="password">
-      </label>
-      <input type="submit">
-      
-    </form>
-  </body>
+	
+	<label> 
+			
+	</label> 
+	</div> 
+</form>
+</body> 
 </html>
-"""
 
-responseForm = """
-<html>
-  <head>
-    <title>BashWiki</title>
-  </head>
-  <body>
-    <p>%(password)s criptografado usando sha256: %(cypher)s</p>
-  </body>
-</html>
+
 """
 
 
@@ -50,5 +72,8 @@ class MainHandler(webapp2.RequestHandler):
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
 ], debug=True)
+
+
+
 
 
